719b057bdaec4c8d8aa5485b1a48de7010cd4218
RajkaranRK <rajkaranrk164@gmail.com>
2022-07-18 15:59:01 +0530

    Adding code

---
+++ src/test/java/com/rk/daa/DaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.daa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
